0ab32c0e6b792a3aa17f149fe433e37c81d08781
Nicklas Lindegaard Martlev Gustavsen <nicklasgustavsen75@gmail.com>
2023-09-13 08:51:11 +0200

    Add project files.

---
+++ H1_TheSortingHat/Model/SortingHat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_TheSortingHat.Model
+{
+    /// <summary>
+    /// Class <see cref="SortingHat"/> has different colleges and makes it possible to fetch them
+    /// </summary>
+    internal class SortingHat
+    {
+        public enum Colleges
+        {
+            Gryffindor = 1,
+            Hufflepuff,
+            Ravenclaw,
+            Slytherin
+        }
+
+        /// <summary>
+        /// Gets a college by id
+        /// </summary>
+        /// <param name="collegeID">id of college</param>
+        /// <returns>The college by id</returns>
+        public Colleges GetCollege(byte collegeID)
+        {
+            return (Colleges)collegeID;
+        }
+    }
+}
---
+++ H1_TheSortingHat/Program.cs
@@ -0,0 +1,21 @@
+﻿using H1_TheSortingHat.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_TheSortingHat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortingHat sortingHat = new SortingHat();
+            Random random = new Random();
+
+            Console.WriteLine("Du tilhører: " + sortingHat.GetCollege((byte)random.Next(1, 5)));
+            Console.ReadLine();
+        }
+    }
+}
